fd3dbfb01a35be88c134e544e4fb7c268fcbcdf3
Minasamy823 <minasamy823@gmail.com>
2020-03-22 21:54:44 +0300

    Initial commit

--- catproject/settings.py
+++ catproject/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 CORS_ORIGIN_ALLOW_ALL = True
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
 #
 # import dj_database_url
 # prod_db  =  dj_database_url.config(conn_max_age=500)
