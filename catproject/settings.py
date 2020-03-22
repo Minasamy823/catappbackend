fc4a4cb875ed2cef0c86b2d6dde5bf81c6e0f701
Minasamy823 <minasamy823@gmail.com>
2020-03-22 22:14:57 +0300

    Initial commit

--- catproject/settings.py
+++ catproject/settings.py
@@ -150,10 +150,11 @@ STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
 
+
 #  Add configuration for static files storage using whitenoise
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
-import dj_database_url
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
+# import dj_database_url
+# prod_db  =  dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(prod_db)
