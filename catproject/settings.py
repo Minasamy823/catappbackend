1dda5ec1d10c668adcb4fbaaa38101e6e14b519a
Minasamy823 <minasamy823@gmail.com>
2020-03-22 21:58:36 +0300

    Initial commit

--- catproject/settings.py
+++ catproject/settings.py
@@ -136,6 +136,6 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 # django_heroku.settings(locals())
 #
-import dj_database_url
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
+# import dj_database_url
+# prod_db  =  dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(prod_db)
