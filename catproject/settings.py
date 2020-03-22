18709c290019c290363ad0ba8d1422005b16fbd5
Minasamy823 <minasamy823@gmail.com>
2020-03-22 22:15:48 +0300

    Initial commit

--- catproject/settings.py
+++ catproject/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'a(fl@y&e(kduch7o3dot4ohrhh84yk@(==8vp+e0%@e9s1^(aq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['cattyapp.herokuapp', 'cattyapp.herokuapp.com']
 
@@ -134,21 +134,21 @@ USE_TZ = True
 
 CORS_ORIGIN_ALLOW_ALL = True
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
 #
 # import dj_database_url
 # prod_db  =  dj_database_url.config(conn_max_age=500)
 # DATABASES['default'].update(prod_db)
 
 
-PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
+# PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
+# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+# STATIC_URL = '/static/'
 
 # Extra lookup directories for collectstatic to find static files
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(PROJECT_ROOT, 'static'),
+# )
 
 
 #  Add configuration for static files storage using whitenoise
